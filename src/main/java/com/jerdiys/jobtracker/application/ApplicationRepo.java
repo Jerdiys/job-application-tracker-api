1dba53b55f07e2c142ff12615ad6bae058945b7c
Jerdiys <seanjed82@gmail.com>
2025-12-25 14:23:01 +0100

    added helper methods to application repo

--- src/main/java/com/jerdiys/jobtracker/application/ApplicationRepo.java
+++ src/main/java/com/jerdiys/jobtracker/application/ApplicationRepo.java
@@ -1,8 +1,14 @@
 package com.jerdiys.jobtracker.application;
 
+import com.jerdiys.jobtracker.job.Job;
+import com.jerdiys.jobtracker.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ApplicationRepo extends JpaRepository<Application, Long> {
+    List<Application> findByCandidate(User candidate);
+    List<Application> findByJob(Job job);
 }
